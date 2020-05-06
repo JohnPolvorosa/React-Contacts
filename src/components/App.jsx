77c361995887f17ba1ed7e04301f5b0025a0dbf5
JohnPolvorosa <johnpolvorosa@ontariotechu.net>
2020-05-05 21:49:28 -0400

    finish challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,49 @@
 import React from "react";
 
+import contacts from "./../contacts.js";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img src={props.img} className="circle-img" alt="{props.alt}" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
+}
+
 function App() {
+  console.log(contacts);
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <body>
+        <h1 className="heading">My Contacts</h1>
+        <Card
+          name={contacts[0].name}
+          img={contacts[0].imgURL}
+          tel={contacts[0].phone}
+          email={contacts[0].email}
+        />
+
+        <Card
+          name={contacts[1].name}
+          img={contacts[1].imgURL}
+          tel={contacts[1].phone}
+          email={contacts[1].email}
+        />
+
+        <Card
+          name={contacts[2].name}
+          img={contacts[2].imgURL}
+          tel={contacts[2].phone}
+          email={contacts[2].email}
+        />
+      </body>
     </div>
   );
 }
